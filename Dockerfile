58d2546de97dd4b869f36d47371afc1d5bd76ed4
Kc-Nell <kirstennell01@gmail.com>
2024-09-13 02:35:28 +0200

    docker added

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# u20780193 - Deliverable 1
+
+FROM node:20
+
+WORKDIR /app
+
+COPY . .
+
+RUN npm install
+
+ENV PORT=3000
+
+CMD [ "npm", "start" ]
+
+EXPOSE 3000
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,6 @@ const app = express();
 app.use(express.static("frontend/public"));
 
 
-app.listen(3000, () => {
-    console.log("Listening on http://localhost:3000");
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost/${process.env.PORT}`);
 });
