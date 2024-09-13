2702c9a42a273205cb893059b55d19a050521759
Kc-Nell <kirstennell01@gmail.com>
2024-09-13 03:25:25 +0200

    docker _fixes_

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,11 +1,16 @@
 "use strict";
 
 var _express = _interopRequireDefault(require("express"));
+var _path = _interopRequireDefault(require("path"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
 // u20780193 - Deliverable 1
 
 var app = (0, _express["default"])();
-app.use(_express["default"]["static"]("frontend/public"));
-app.listen(3000, function () {
-  console.log("Listening on http://localhost:3000");
+var _dirname = _path["default"].resolve();
+app.use(_express["default"]["static"](_path["default"].join(_dirname, "frontend/build")));
+app.get("*", function (req, res) {
+  res.sendFile(_path["default"].join(_dirname, "frontend/build", "index.html"));
+});
+app.listen(process.env.PORT, function () {
+  console.log("Server is running on http://localhost:".concat(process.env.PORT));
 });
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 // u20780193 - Deliverable 1
 
 import express from "express";
+import path from "path";
 
 const app = express();
 
-app.use(express.static("frontend/public"));
+const __dirname = path.resolve();
 
+app.use(express.static(path.join(__dirname, "frontend/public")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/public", "index.html"));
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on http://localhost/${process.env.PORT}`);
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
